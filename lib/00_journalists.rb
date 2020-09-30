7d76e21047f2da3f6d88cff7c41a478b22cf1470
florianempinet <fempinet@gmail.com>
2020-09-30 18:12:02 +0200

    Premier Commit

---
+++ lib/00_journalists.rb
@@ -0,0 +1,145 @@
+@journalist = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+
+  def number_in_arrays()
+  puts "A l'intérieur de ce tableau, il y a #{@journalist.length()} compte Twitter"
+  end
+
+  def most_short
+    puts "Celui qui a la plus petite, c'est #{@journalist.min_by(&:length)}.. désolé gros"
+  end
+  
+  
+  def five_caracter
+        puts "Dans le tableau, il y a #{@journalist.count {|c| c.length == 6}} comptes avec 5 caractères"
+  end 
+  
+  
+  
+  # Ca c'est ok !
+  def uppercase_journalist
+    puts "Il y a #{@journalist.count{|u| u =~ /^@[[:upper:]]/}} handle qui commencent par une majuscule."
+  end
+  
+  
+  # Ca c'est ok !
+  def alphabetic_journalist
+    puts "Classer les noms par ordre alphabétique #{@journalist.sort_by(&:downcase)}}"
+  end
+  
+  
+  # Ca c'est ok ! 
+  def sort_by_size
+    puts "Voici les comptes par ordre de grandeur du pseudo #{@journalist.sort_by(&:length)}"
+  end
+  
+  
+  # Ca c'est ok ! 
+  def where_is_epenser()
+    puts "@Epenser est à la place #{@journalist.find_index("@epenser")} dans le tableau"
+  end
+  
+  
+  
+  def by_caracters 
+   hand_size = Array.new 
+   @journalist.each do |handle|
+    hand_size << handle.length
+   end
+   for i in hand_size.min..hand_size.max
+    puts "Il y a #{hand_size.count(i)} handles qui possèdent #{i-1} caractères."
+   end
+  
+  end
+  
+
+  def start 
+    puts "Bienvenue sur le Excell du Pauvre ! J'ai un tableau de plein de compte Twitter, je peux te proposer plein de trucs."
+    print "Est-ce que tu veux :"
+    print "
+    ========================================"
+    print "
+    Savoir combien il y a de handle dans le tableau (1)
+    Savoir qui a la plus petite (2)
+    Savoir combien ont 5 caractères dans leurs pseudos (3)
+    Savoir combien commencent par une majuscule (4)
+    Trier la liste par ordre alphabétique (5)
+    Trier la liste par taille de pseudos (6)
+    Savoir où est @Epenser (7)
+    Avoir une répartition des pseudos par taille de pseudo (8)"
+    
+    print "
+    ========================================"
+    
+    puts "
+    Vous envoyez le numéro de votre choix AU 74 900 (prix du SMS + 75€)"
+
+
+  case (gets.chomp)
+    when '1'
+      number_in_arrays
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    when '2'
+      most_short
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    when '3'
+      five_caracter
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    when '4'
+      uppercase_journalist
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    when '5'
+      alphabetic_journalist
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    when '6'
+      sort_by_size
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    when '7'
+      where_is_epenser
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    when '8'
+      by_caracters
+      puts " "
+      puts "Pour relancer une partie : Appuie sur ENTREE"
+      gets.chomp
+      start
+    else 
+      puts "Euh.. j'ai pas encore codé ça.."
+end
+end 
+start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
